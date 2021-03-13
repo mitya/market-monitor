f0e1dd7e5e7466bb10128e9deb845cebe098bdeb
Dmitry Sokurenko <dmitry@sokurenko.name>
2021-03-13 15:54:28 +0300

    Mute the currency sign

--- app/helpers/instruments_helper.rb
+++ app/helpers/instruments_helper.rb
@@ -2,15 +2,20 @@ module InstrumentsHelper
   def change_percentage(relation)
   end
 
-  def colorize_change(value, green: nil, format: :number)
+  def colorize_change(value, green: nil, format: :number, title: nil)
     green = value > 0 if green == nil && value.is_a?(Numeric)
     value = number_to_currency value if format == :number
     value = number_to_percentage value * 100, precision: 2 if value && format == :percentage
-    tag.span(value, class: "changebox changebox-#{green ? 'green' : 'red'}")
+    tag.span(value, class: "changebox changebox-#{green ? 'green' : 'red'}", title: title)
+    tag.span(number_to_currency(title), class: "changebox changebox-#{green ? 'green' : 'red'}")
   end
 
   def currency_sign(currency_code)
     CurrencySigns[currency_code.to_s.to_sym]
+  end
+
+  def currency_span(currency_code)
+    tag.span currency_sign(currency_code), class: 'currency'
   end
 
   CurrencySigns = { USD: '$', RUB: '₽', EUR: '€' }
