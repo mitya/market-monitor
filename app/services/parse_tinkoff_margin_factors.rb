e59d4fa641d268b1c2880dc6e4714354318a0c88
Dmitry Sokurenko <dmitry@sokurenko.name>
2022-05-25 00:43:26 +0300

    Update T margin factors

--- app/services/parse_tinkoff_margin_factors.rb
+++ app/services/parse_tinkoff_margin_factors.rb
@@ -1,20 +1,27 @@
+# https://www.tinkoff.ru/invest/margin/equities/
 class ParseTinkoffMarginFactors
   include StaticService
 
-  def run
+  def run(real = false)
     doc = Nokogiri::HTML open("db/data/tinkoff-margin-factors.html"), nil, "UTF-8"
 
-    doc.css('.Table__table_14Vfq tbody tr').each do |node|
-      title = node.css('.LiquidPapersPure__subtitle_ggFbH').first.content
-      isin = node.css('.LiquidPapersPure__isin_1N24p').first.content
-      can_short = node.css('td:nth-child(3) .Table__linkCell_38tXR').first.content == 'Доступен'
-      values = node.css('td:nth-child(4) .Table__linkCell_38tXR').first.content
+    doc.css("tr[data-qa-type='uikit/table.tableRow']").each do |node|
+      title = node.css('.LiquidPapersPure__subtitle_z6_Py').first&.content
+      next if not title
+      isin = node.css('td:nth-child(2)').first.content
+      can_short = node.css('td:nth-child(3)').first.content == 'Доступен'
+      values = node.css('td:nth-child(4)').first.content
       ticker = title.split(',').first
       long_k, short_k = values.split('/').map { |v| v.squish.to_f }
-      # puts "#{ticker} #{long_k} #{short_k}"
+
+      # puts "#{title} #{isin} #{can_short} #{values}"
+      # printf "%6s #20s %2d %2d %s\n", ticker, isin, long_k, short_k, can_short
+      p({ isin:, ticker:, long_k:, short_k:, can_short: })
+      next
 
       instrument = Instrument[ticker]
       next puts "Missing #{ticker}".red if instrument == nil
+      next unless real
       instrument.info!.update! extra: instrument.info!.extra.to_h.merge({
         tinkoff_long_risk: long_k,
         tinkoff_short_risk: short_k,
